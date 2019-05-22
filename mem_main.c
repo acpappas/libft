9ce2da6f9349b2adc0e99be8e89405f1f061abbf
Alexandra PAPPAS <apappas@c3r2s2.wethinkcode.co.za>
2019-05-22 13:24:19 +0200

    first set of changes made

---
+++ mem_main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mem_main.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apappas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 07:53:57 by apappas           #+#    #+#             */
+/*   Updated: 2019/05/22 09:17:36 by apappas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+/*test for memset
+
+int		main()
+{
+	char str[50] = "GeeksForGeeks is for programming geek.";
+	printf("\nBefore memset(): %s\n", str);
+
+	memset(str + 13, '.', 8*sizeof(char));
+
+	printf("After memset(): %s", str);
+	return 0;
+}
+
+test for bzero
+
+int		main()
+{
+	size_t c = 2;
+	char str[] = "Hello";
+	
+	printf("Before bzero: %s\n", str);
+		
+	bzero(str, c);
+	printf("After bzero: %s\n", str);
+
+return 0;
+}
+*/
+
+int		main()
+{
+
