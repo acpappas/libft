4917bd6194abe1170773d657d0d7263a853f79a1
Alexandra PAPPAS <apappas@c3r2s2.wethinkcode.co.za>
2019-05-24 09:12:15 +0200

    added strlen and added header to memset

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apappas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/24 09:06:12 by apappas           #+#    #+#             */
-/*   Updated: 2019/05/24 09:06:19 by apappas          ###   ########.fr       */
+/*   Created: 2019/05/24 08:29:16 by apappas           #+#    #+#             */
+/*   Updated: 2019/05/24 09:04:09 by apappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+size_t	strlen(const char *s)
 {
-	unsigned char *ptr = b;
-
-	while (len > 0)
-	{
-		*ptr = (unsigned char) c;
-		len--;
-		*ptr++;
-	}
-	return b;
+	size_t i;
+	i = 0;
+	while(s[i] != '\0')
+		i++;
+	return i;
+}
