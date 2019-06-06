95da58993cba35df5c20bd8306b3def6f5e5a394
Alexandra PAPPAS <apappas@c3r4s5.wethinkcode.co.za>
2019-06-06 16:17:40 +0200

    worked on str functions

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,15 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apappas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/06 09:24:08 by apappas           #+#    #+#             */
-/*   Updated: 2019/06/06 15:28:34 by apappas          ###   ########.fr       */
+/*   Created: 2019/06/06 15:41:05 by apappas           #+#    #+#             */
+/*   Updated: 2019/06/06 16:01:48 by apappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char *))
-{
+#include <string.h>
 
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s1 && *s2)
+		if (s1[i] == s2[i])
+			i++;
+	return (0);
+		else if(s1[i] > s2[i])
+			return(s1[i] - s2[i]);
+		else if(s1[i] < s2[i])
+			return(s2[i] - s1[i]);
+}
