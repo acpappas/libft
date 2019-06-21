5c6ac3147336fd9c0546040ba96bfede84d8d9c6
Xandra <xandra.pappas@gmail.com>
2019-06-21 22:13:37 +0200

    finished ft_strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,35 +12,44 @@
 
 #include "libft.h"
 
+static char	**ft_split(char const *s, char c, char **splstr, int count)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (*(s + 1) != '\0' && i < count)
+	{
+		while (*s == c && *(s + 1) != '\0')
+			s++;
+		splstr[i] = (char *)malloc(sizeof(char) * (ft_wlength(s, c) + 1));
+		while (*s != c && *s)
+		{
+			*(*(splstr + i) + j) = *s;
+			s++;
+			j++;
+		}
+		*(*(splstr + i) + j) = '\0';
+		i++;
+		j = 0;
+	}
+	*(splstr + i) = 0;
+	return (splstr);
+}
+
 char                    **ft_strsplit(char const *s, char c)
 {
          int             count;
          char            **splstr;
 
- 
+ 	count = 0;
         if (!s)
                  return (NULL);
          count = ft_wcount(s, c);
-         splstr = (char **)malloc(sizeof(char *) * (count + 1));
+         splstr = ((char **)malloc(sizeof(char *) * (count + 1)));
          if (splstr == NULL)
                  return (NULL);
-        count = 0;
-         while (*s)
-         {
-                 while (*s == c && *s != '\0')
-                         s++;       
-                 splstr[count]  = (char *)malloc(sizeof(char) * (ft_wlength(s, c) + 1));
-                 
-                 while (*s != c && *s)
-                 {
-                         *(*(splstr + count)) = *s;
-                         *(splstr + count) += 1;
-                         s++;
-                 }
-                 *(*(splstr + count)) = '\0';
-                 count++;
-                 
-         }
-         *(splstr + count) = 0;
+         splstr = ft_split(s, c, splstr, count);
          return (splstr);
 }
