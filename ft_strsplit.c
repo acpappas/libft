9333f97c2e0cf485669176d4b41d99ebed455125
Alexandra PAPPAS <apappas@c3r4s5.wethinkcode.co.za>
2019-06-21 17:46:58 +0200

    worked on strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: apappas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 14:56:02 by apappas           #+#    #+#             */
-/*   Updated: 2019/06/17 12:21:48 by apappas          ###   ########.fr       */
+/*   Updated: 2019/06/21 17:37:54 by apappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,29 @@ char	**ft_strsplit(char const *s, char c)
 	int		count;
 	int		i;
 
-	i = 0;
 	count = 0;
-	if (!s || !c)
+	if (!s)
 		return (NULL);
 	count = ft_wcount(s, c);
 	splstr = ((char **)malloc(sizeof(char *) * (count + 1)));
 	if (splstr == NULL)
 		return (NULL);
-	while (i < count)
+	i = 0;
+	while (*(s + 1) != '\0' && i < count)
 	{
-		while (*s == c && *s)
+		while (*s == c && *(s + 1) != '\0')
 			s++;
 		splstr[i] = (char *)malloc(sizeof(char) * (ft_wlength(s, c) + 1));
-		while (*s != c && *s)
-			*splstr[i]++ = *s++;
-		*splstr[i] = '\0';
+		while (*s != c && *(s + 1) != '\0')
+		{
+			*(*(splstr + i)) = *s;
+			*(splstr + i) += 1;
+			s++;
+		
+		}
+		*(*(splstr + i)) = '\0';
 		i++;
 	}
-	*splstr[i] = '\0';
+	*(splstr + i) = 0;
 	return (splstr);
 }
